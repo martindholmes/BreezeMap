f560632944e57d22e8b28691d950366e0a226572
martindholmes <mholmes@uvic.ca>
2021-04-09 13:28:52 -0700

    Update publish script.

--- maps_to_server.sh
+++ maps_to_server.sh
@@ -2,7 +2,7 @@
 
 echo "Building test projects..."
 
-cd utilities && ant && cd ../
+ant
 
 echo "Pushing changes up to the hcmc account on nfs.hcmc.uvic.ca using rsync..."
-rsync --verbose --progress --stats --compress --recursive --times --perms --delete test/ hcmc@nfs.hcmc.uvic.ca:/home1t/hcmc/www/maps/
+rsync --verbose --progress --stats --compress --recursive --times --perms --delete testsite/ hcmc@nfs.hcmc.uvic.ca:/home1t/hcmc/www/maps/
